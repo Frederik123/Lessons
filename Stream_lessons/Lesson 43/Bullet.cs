9b65e518bc78a93bfb825a218145844fe7ac5b2a
Frederik123 <for_frederik@mail.ru>
2020-04-13 17:02:41 +0300

    43

---
+++ Stream_lessons/Lesson 43/Bullet.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 925f; //Скорость пули м/c
+    public float windHSpeed = 0f; //Сила бокового ветра м/с
+    public float windVSpeed = 0f; //Сила продольного ветра м/с
+    public float temp = 15f; //Температура воздуха
+    public float pressure = 750f; //Атмосферное давление
+
+    public float rightDerivation = 0.2f; //Деривация м/с
+    public float hWindCoeff = .05f; //Коэффициэнт на боковой ветер
+    public float vWindCoeff = .025f; //Коэффициэнт на продольный ветер
+    public float tempCoeff = .03f; //Коэффициэнт на температуру +/-1С от 15
+    public float pressureCoeff = .01f; //Коэффициэнт на атмосферное давление +/1 мм.
+
+    public GameObject bulletDecal;
+
+    private float tempReference = 15f;
+    private float pressureReference = 750f;
+    private float currentSpeed;
+    private float timeOfFly = 0;
+    private bool destroyed = false;
+
+    void Start()
+    {
+        currentSpeed = speed;
+    }
+
+    void Update()
+    {
+        if (!destroyed)
+        {
+            // движение пули в зависимости от "погодных" условий
+            Vector3 oldPosition = transform.position;
+            var delta = Time.deltaTime;
+            var t = temp - tempReference;
+            var p = pressure - pressureReference;
+            var coefficient = (transform.right * rightDerivation * delta) //деривация
+                + (transform.right * (windHSpeed * hWindCoeff * delta)) //боковой ветер
+                + (transform.up * (windVSpeed * vWindCoeff * delta + t * tempCoeff * delta - p * pressureCoeff * delta)) // продольный ветер, температура и давление
+                ;
+            transform.position += transform.forward * currentSpeed * delta + coefficient;
+            timeOfFly += delta;
+            currentSpeed -= delta * (pressure * .5f + windVSpeed);
+            RaycastHit hit;
+
+
+            if (Physics.Raycast(oldPosition, transform.position - oldPosition, out hit, Vector3.Distance(oldPosition, transform.position)))
+            {
+                transform.position = hit.point;
+                //Во что-то попали)
+                if (hit.transform.name != "Terrain")
+                    //Destroy(hit.transform.gameObject);
+
+                    if (bulletDecal != null)
+                    {
+                        /*
+                        GameObject decal = GameObject.Instantiate<GameObject>(bulletDecal);
+                        decal.transform.position = hit.point + hit.normal * .003f;
+                        decal.transform.rotation = Quaternion.FromToRotation(decal.transform.forward, hit.normal);
+                        decal.transform.Rotate(decal.transform.forward, Random.Range(0, 360));
+                        decal.transform.SetParent(hit.transform);
+                        */
+                        GameObject decal = Instantiate<GameObject>(bulletDecal); // инициализируем дырку
+                        decal.transform.position = hit.point + hit.normal * 0.01f + new Vector3(0, 0.05f, 0.00f);
+                        decal.transform.rotation = Quaternion.LookRotation(-hit.normal);
+                        decal.transform.SetParent(hit.transform);
+                    }
+
+                currentSpeed = 0;
+                destroyed = true;
+            }
+
+            if (timeOfFly > 3f || currentSpeed <= 0) Destroy(gameObject);
+        }
+    }
+}
---
+++ Stream_lessons/Lesson 43/Sniper.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sniper : MonoBehaviour
+{
+    [Header("Настройки камеры")]
+    public Camera cam;
+    [Space]
+
+    [Header("Настройки прицела")]
+    public float correctH = 0; //Горизонтальная корректировка прицела
+    public float correctV = 0; //Вертикальная корректировка прицела
+    public float correctStep = .01f; //Шаг корректировки прицела
+    public float returnOffset = .02f; //Смещение на отдачу
+    [Space]
+
+    [Header("Ссылки на префабы и компоненты")]
+    [Tooltip("Префаб пули")]
+    public GameObject bullet;
+
+    [Tooltip("Ссылка на точку в пространстве для респавна пули ")]
+    public GameObject bullet_respawn;
+
+    [Tooltip("Проигрыватель")]
+    public AudioSource sounds;
+
+    [Tooltip("Звук выстрела")]
+    public AudioClip soundFire;
+    [Space]
+
+    private bool busy = false; //Занято, управление не работает (время отдачи)
+    private Quaternion oldRotation; //Старый поворот (куда должна вернуться камера после отдачи)
+
+
+    public GameObject sniper_rufle;
+    private Vector3 sniper_pos_start;
+    private Vector3 sniper_pos_end = new Vector3(-0.012f, -0.326f, 0.316f); // координаты прицела при прицеливании
+
+    void Start()
+    {
+        sniper_pos_start = sniper_rufle.transform.localPosition;
+    }
+
+    void Update()
+    {
+
+        // прицеливаемся 
+        if (Input.GetMouseButtonDown(1))
+        {
+            StartCoroutine(SmoothMove(sniper_rufle, sniper_pos_start, sniper_pos_end, 0.3f));
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            StartCoroutine(SmoothMove(sniper_rufle, sniper_pos_end, sniper_pos_start, 0.3f));
+        }
+
+
+        if (!busy && cam != null)
+        {
+            //Стрельба
+            if (Input.GetMouseButtonDown(0))
+            {
+                //Звук выстрела
+                if (sounds != null && soundFire != null)
+                    sounds.PlayOneShot(soundFire);
+
+                if (bullet != null)
+                {
+                    //Инстанциируем пулю
+                    // var bulletInstance = (GameObject)GameObject.Instantiate(bullet, cam.transform.position, cam.transform.rotation);
+                    var bulletInstance = (GameObject)GameObject.Instantiate(bullet, bullet_respawn.transform.position, cam.transform.rotation);
+
+                    //Поворачиваем в зависимости от корректировки прицела
+                    bulletInstance.transform.rotation *= Quaternion.Euler(correctV, correctH, 0f);
+
+                    //Отдача
+                    oldRotation = transform.rotation * Quaternion.Euler(Random.Range(-returnOffset, returnOffset), Random.Range(-returnOffset, returnOffset), 0f);
+                    var newRotation = oldRotation.eulerAngles - transform.right * 1f + transform.up * .2f;
+                    transform.rotation = Quaternion.Euler(newRotation);
+                    busy = true;
+                    StartCoroutine(ReturnRotation());
+                }
+            }
+        }
+    }
+
+    // плавное прицеливание
+    IEnumerator SmoothMove(GameObject gameObject, Vector3 startPos, Vector3 endPos, float time)
+    {
+        float currTime = 0;
+        do
+        {
+            gameObject.transform.localPosition = Vector3.Lerp(startPos, endPos, currTime / time);
+            currTime += Time.deltaTime;
+            yield return null;
+        }
+        while (currTime <= time);
+    }
+
+    // Возврат после отдачи
+    IEnumerator ReturnRotation()
+    {
+        while (true)
+        {
+            if (Quaternion.Angle(transform.rotation, oldRotation) <= .001f)
+            {
+                transform.rotation = oldRotation;
+                break;
+            }
+            transform.rotation = Quaternion.Lerp(transform.rotation, oldRotation, Time.deltaTime * 7f);
+            yield return null;
+        }
+        busy = false;
+    }
+}
