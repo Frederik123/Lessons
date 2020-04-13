911c6c28ded70e9d58b7e976c8ed0baf359d8a54
Frederik123 <for_frederik@mail.ru>
2020-04-13 16:52:14 +0300

    25

---
+++ Stream_lessons/Lesson 25-1/Car.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Car : MonoBehaviour
+{
+    [Tooltip("массив передних колёс c wheelcollider")]
+    public WheelCollider[] frontCols; // массив передних колёс c wheelcollider
+    [Tooltip("массив отображаемых передних колёс ")]
+    public Transform[] dataFront; // массив отображаемых передних колёс 
+    public WheelCollider[] backCol; // массив задних колёс c wheelcollider
+    public Transform[] dataBack; // массив отображаемых задних колёс 
+    public Transform centerOfMass;  // объект по которому будет задаваться центр массы автомобиля
+
+
+    public float maxSpeed = 30f; // макс скорость
+    private float sideSpeed = 30f; // скорость поворота и угол градуса поворота колёс
+    public float breakSpeed = 100f; // сила торможения
+
+    private Sounds sound;
+
+
+
+    void Start()
+    {
+        // задаём объект по которому будет браться центр массы (смещяем вниз авто)
+        // https://docs.unity3d.com/ScriptReference/Rigidbody-centerOfMass.html
+        GetComponent<Rigidbody>().centerOfMass = centerOfMass.localPosition;  
+
+        sound = gameObject.GetComponent<Sounds>(); // ссылка на скрипт со звуками
+    }
+
+    void Update()
+    {
+        /** Get axis **/
+        float vAxis = Input.GetAxis("Vertical");
+        float hAxis = Input.GetAxis("Horizontal");
+        bool brakeButton = Input.GetButton("Jump");
+        /** End get axis **/
+
+        /** Motor - двигаем вперёд-назад **/
+        // motorTorque - крутящий момент колеса.  
+        // изменяем motorTorque, умножая её на значение полученное с вертикальной оси, вследствие чего наш автомобиль начинает ускоряться.
+        frontCols[0].motorTorque = vAxis * maxSpeed;  
+        frontCols[1].motorTorque = vAxis * maxSpeed;
+        /** End motor **/
+
+        /** Brake - тормоза **/
+        if (brakeButton)
+        {
+            // brakeTorque - торможение колеса
+            frontCols[0].brakeTorque = Mathf.Abs(frontCols[0].motorTorque) * breakSpeed;
+            frontCols[1].brakeTorque = Mathf.Abs(frontCols[1].motorTorque) * breakSpeed;
+        }
+        else
+        {
+            frontCols[0].brakeTorque = 0;
+            frontCols[1].brakeTorque = 0;
+        }
+        /** End brake **/
+
+        /** Rotate **/
+        // поворот влево-вправо
+        // steerAngle - Угол поворота в градусах, всегда вокруг локальной оси Y.
+        frontCols[0].steerAngle = hAxis * sideSpeed;
+        frontCols[1].steerAngle = hAxis * sideSpeed;
+        /** End rotate **/
+
+        /** Update graphics cols **/
+        // меняем графику отображаемых колёс
+        // rpm - текущая скорость вращения колеса
+       
+        dataFront[0].Rotate(frontCols[0].rpm * Time.deltaTime, 0, 0);
+        dataFront[1].Rotate(frontCols[1].rpm * Time.deltaTime, 0, 0);
+        dataBack[0].Rotate(backCol[0].rpm * Time.deltaTime, 0, 0);
+        dataBack[1].Rotate(backCol[1].rpm * Time.deltaTime, 0, 0);
+  /*       
+        dataFront[0].Rotate(0, 0, -frontCols[0].rpm * Time.deltaTime);
+        dataFront[1].Rotate(frontCols[1].rpm * Time.deltaTime, 0, 0);
+        dataBack[0].Rotate(backCol[0].rpm * Time.deltaTime, 0, 0);
+        dataBack[1].Rotate(backCol[1].rpm * Time.deltaTime, 0, 0);
+*/
+        /*
+        dataFront[0].rotation *= Quaternion.Euler(frontCols[0].rpm * Time.deltaTime, hAxis * sideSpeed, 0f);
+        dataFront[1].rotation *= Quaternion.Euler(frontCols[1].rpm * Time.deltaTime, hAxis * sideSpeed, 0f);
+        dataBack[0].rotation *= Quaternion.Euler(backCol[0].rpm * Time.deltaTime, 0f, 0f);
+        dataBack[1].rotation *= Quaternion.Euler(backCol[1].rpm * Time.deltaTime, 0f, 0f);
+        */
+
+
+
+        // меняем плавно анимацию передних колёс. только по оси Y (вправо-влево)
+        // не знаю почему, но колесо периодически разворачивается на 180 градусов по Z
+        dataFront[0].localEulerAngles = new Vector3(dataFront[0].localEulerAngles.x, hAxis * 30f, dataFront[0].localEulerAngles.z);
+        dataFront[1].localEulerAngles = new Vector3(dataFront[1].localEulerAngles.x, hAxis * sideSpeed, dataFront[1].localEulerAngles.z);
+
+        /*
+        Debug.Log(dataFront[0].localEulerAngles.z);
+
+        if (dataFront[0].localEulerAngles.z == 180)
+        {
+            Debug.Log("here");
+            dataFront[0].Rotate(dataFront[0].transform.rotation.x, 0, 0);
+        }
+        */
+
+        /* End update graphics cols **/
+
+        /** Skid - озвучка заноса **/
+        WheelHit hit;
+        // GetGroundHit - Получает данные о столкновении с землей для колеса.
+        if (backCol[0].GetGroundHit(out hit))
+        {
+            // sidewaysSlip - боковой занос
+            float vol = (Mathf.Abs(hit.sidewaysSlip) > .25f) ? hit.sidewaysSlip / 2.5f : 0;
+            sound.playSkid(vol);
+        }
+        /** End skid **/
+    }
+}
---
+++ Stream_lessons/Lesson 25-2/CarController.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour
+{
+	[Header("Массив Wheel collider")]
+	[Tooltip("Передние")]
+	public WheelCollider[] WColForward;
+	[Tooltip("Задние")]
+	public WheelCollider[] WColBack;
+
+	[Header("Массив видимых колёс")]
+	[Tooltip("Передние")]
+	public Transform[] wheelsF; //1
+	[Tooltip("Задние")]
+	public Transform[] wheelsB; //1
+
+	[Header("Прочие параметры")]
+	public float wheelOffset = 0.1f; //2
+	public float wheelRadius = 0.5f; //2
+
+	[Tooltip("Максимальный угол поворота колёс")]
+	public float maxSteer = 30f;
+	[Tooltip("Ускорение")]
+	public float maxAccel = 250f;
+	[Tooltip("Торможение")]
+	public float maxBrake = 50f;
+
+	[Tooltip("Ссылка на центр массы")]
+	public Transform COM;
+
+	public class WheelData
+	{
+		public Transform wheelTransform;
+		public WheelCollider col;
+		public Vector3 wheelStartPos;
+		public float rotation = 0.0f;
+	}
+
+	protected WheelData[] wheels;
+
+	// Use this for initialization
+	void Start()
+	{
+		GetComponent<Rigidbody>().centerOfMass = COM.localPosition;
+
+		wheels = new WheelData[WColForward.Length + WColBack.Length];
+
+		for (int i = 0; i < WColForward.Length; i++)
+		{
+			wheels[i] = SetupWheels(wheelsF[i], WColForward[i]);
+		}
+
+		for (int i = 0; i < WColBack.Length; i++)
+		{
+			wheels[i + WColForward.Length] = SetupWheels(wheelsB[i], WColBack[i]);
+		}
+
+	}
+
+	// Функция SetupWheels() принимает Transform колеса и его WheelCollider, 
+	// передает в переменные содержащиеся в классе WheelData необходимые нам данные и возвращает его.
+	private WheelData SetupWheels(Transform wheel, WheelCollider col)
+	{
+		WheelData result = new WheelData();
+
+		result.wheelTransform = wheel;
+		result.col = col;
+		result.wheelStartPos = wheel.transform.localPosition;
+
+		return result;
+	}
+
+	void FixedUpdate()
+	{
+		float accel = 0;
+		float steer = 0;
+
+		accel = Input.GetAxis("Vertical");
+		steer = Input.GetAxis("Horizontal");
+
+		CarMove(accel, steer);
+		UpdateWheels();
+	}
+
+	// движение видимых колёс
+	private void UpdateWheels()
+	{
+		float delta = Time.fixedDeltaTime;
+
+		foreach (WheelData w in wheels)
+		{
+			WheelHit hit; // в которой в свою очередь содержится точка соприкосновения коллайдера и террейна (переменная point)
+
+			Vector3 lp = w.wheelTransform.localPosition; // локальная позиция колеса
+			if (w.col.GetGroundHit(out hit)) // Если WheelCollider колеса сталкивается с поверхностью террейна
+			{
+				lp.y -= Vector3.Dot(w.wheelTransform.position - hit.point, transform.up) - wheelRadius;
+				// То из координаты Y локальной позиции колеса вычитаем Dot() между вектором с началом в точке 
+				// в которой коллайдер соприкасается с поверхностью террейна(hit.point) и концом в текущей позиции 
+				// колеса(w.wheelTransform.position) и между вектором направленным вверх относительно объекта 
+				// Car(transform.up) и из всего этого вычитаем еще и переменную wheelRadius чтобы колесо заняло правильное место
+			}
+			else
+			{
+				lp.y = w.wheelStartPos.y - wheelOffset;
+				// Если WheelCollider не касается поверхности террейна. То из координаты Y начальной локальной позиции 
+				// колеса отнимаем wheelOffset, благодаря этому наши колеса не улетают в неизвестном направлении 
+				// когда автомобиль падает с высоты или лежит на «спине»;
+			}
+			w.wheelTransform.localPosition = lp; // Применяем измененную позицию колеса к его текущей позиции
+
+			w.rotation = Mathf.Repeat(w.rotation + delta * w.col.rpm * 360.0f / 60.0f, 360.0f); // угол «вращения» колеса
+			//w.wheelTransform.localRotation = Quaternion.Euler(w.rotation, w.col.steerAngle, 90.0f); //21
+			// Применяем к текущим локальным углам поворота получившийся результат
+			w.wheelTransform.localRotation = Quaternion.Euler(w.rotation, w.col.steerAngle, 0.0f);
+		}
+
+	}
+
+	private void CarMove(float accel, float steer)
+	{
+		foreach (WheelCollider col in WColForward)
+		{
+			col.steerAngle = steer * maxSteer; // поворот влево-вправо
+		}
+
+		if (accel == 0)
+		{
+			foreach (WheelCollider col in WColBack)
+			{
+				col.brakeTorque = maxBrake; // brakeTorque - торможение колеса
+			}
+		}
+		else
+		{
+			foreach (WheelCollider col in WColBack)
+			{
+				col.brakeTorque = 0;
+				col.motorTorque = accel * maxAccel; // motorTorque - крутящий момент колеса.  
+			}
+		}
+	}
+}
