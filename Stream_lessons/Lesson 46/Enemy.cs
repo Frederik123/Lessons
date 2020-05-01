d151d1da394fc037750a90a3acee75fd0082568c
Frederik123 <for_frederik@mail.ru>
2020-05-01 14:34:08 +0300

    46
    Урок 46 :  2D Melee Combat

---
+++ Stream_lessons/Lesson 46/Enemy.cs
@@ -0,0 +1,36 @@
+﻿// вешаем на противника
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth = 100;
+    int currentHealth;
+
+    public Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth; 
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        animator.SetTrigger("Hurt");
+
+        if (currentHealth <= 0) // умирание проитвника
+        {
+            animator.SetTrigger("Death");
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static; // чтобы не улетало за текстуры
+            GetComponent<BoxCollider2D>().enabled = false;
+            this.enabled = false;
+        }
+    }
+
+
+}
---
+++ Stream_lessons/Lesson 46/PlayerCombat.cs
@@ -0,0 +1,64 @@
+﻿// вешаем на игрока . скрипт атаки
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 30;
+    private Animator m_animator;
+
+    public float attackSpeed = 2f;
+    float nextAttackTime = 0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nextAttackTime -= Time.deltaTime;
+    }
+
+
+    public void Attack()
+    {
+        if (nextAttackTime > 0)
+            return;
+
+        // анимация
+        m_animator.SetTrigger("Attack");
+
+        // враг в радиусе 
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        // дамаг по врагам в радиусе
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("Ударили : " + enemy.name);
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+
+        // время следующей атаки
+        nextAttackTime = attackSpeed; 
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange); 
+    }
+
+}
